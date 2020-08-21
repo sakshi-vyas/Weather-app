1569f68b92a1e8bbe675a08f56b1e497fbcf82a9
sakshi-vyas <sakshivyas216@gmail.com>
2020-08-22 01:04:03 +0530

    Weather-App

---
+++ app.js
@@ -0,0 +1,56 @@
+window.addEventListener('load',() => {
+let long;
+let lat;
+let temperatureDescription=document.querySelector(".temperature-description");
+let temperatureDegree=document.querySelector(".temperature-degree");
+let locationTimezone=document.querySelector(".location-timezone");
+let temperatureSection=document.querySelector('.temperature');
+const temperatureSpan=document.querySelector(".temperature span");
+
+if(navigator.geolocation){
+navigator.geolocation.getCurrentPosition(position => {
+    long = position.coords.longitude;
+    lat= position.coords.latitude;
+    const proxy= `https:\\cors-anywhere.herokuapp.com/`;
+    const api=`${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
+    fetch(api).then(response =>{return response.json();})
+.then(data =>{
+     const {temperature,summary,icon}=data.currently;
+    //set dom elements from api
+    temperatureDegree.textContent= temperature;
+        locationTimezone.textContent= data.timezone;
+        console.log(summary);
+        temperatureDescription.textContent=summary;
+        
+        //temperatureDescription.textContent = summary;
+    // formula for celsius
+    let celcius = (temperature-32)*(5/9);
+// set icon
+setIcons(icon,document.querySelector('.icon'));
+
+// change temperature to celsius/fahrenheit
+temperatureSection.addEventListener('click',()=>{
+    if(temperatureSpan.textContent==='F'){
+        temperatureSpan.textContent='C';
+        temperatureDegree.textContent = Math.floor(celcius);
+    }
+    else{
+        temperatureSpan.textContent='F';
+        temperatureDegree.textContent = temperature;
+
+    }
+});
+
+
+
+});
+});
+}
+
+function setIcons(icon,iconId){
+    const skycons=new Skycons({color:"blue"});
+    const currentIcon=icon.replace(/-/g,"_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconId,Skycons[currentIcon]);
+}
+});
